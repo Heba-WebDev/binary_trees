37743e3150adc1dfb79a15e00b904902c3fbc299
Heba Omar <jtvonline@yahoo.com>
2023-08-31 22:05:40 +0300

    A function that checks if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -49,3 +49,39 @@ return (x * _pow_recursion(x, y - 1));
 }
 
 }
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: the tree to measure its size
+ * Return: If tree is NULL, the function must return 0
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+if (!tree)
+return (0);
+
+
+return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: the tree to measure its height
+ * Return: If tree is NULL, your function must return 0
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t leftNodes = 0;
+size_t rightNodes = 0;
+
+if (!tree)
+return (0);
+
+leftNodes = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+rightNodes = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+return (leftNodes > rightNodes ? leftNodes : rightNodes);
+
+}
